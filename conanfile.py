5e9311e102976538e7b948e59f680958b00b1ec0
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:22:53 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BoostFunction_TypesConan(ConanFile):
     version = "1.64.0"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-function_types"
-    source_url = "https://github.com/boostorg/function_types"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level5Group/1.64.0@bincrafters/testing"
