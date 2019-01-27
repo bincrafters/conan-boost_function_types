9f44111e8e971b9f1942222c7c08e4c7a98ed30f
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:43 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostFunction_TypesConan(base.BoostBaseConan):
     name = "boost_function_types"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_function_types"
     lib_short_names = ["function_types"]
     header_only_libs = ["function_types"]
