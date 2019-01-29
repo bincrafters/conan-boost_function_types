46c5573e9ec25a1ba8a674349aa412b16e55d043
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:09 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostFunction_TypesConan(base.BoostBaseConan):
     name = "boost_function_types"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_function_types"
     lib_short_names = ["function_types"]
     header_only_libs = ["function_types"]
